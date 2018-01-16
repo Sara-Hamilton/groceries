a3e3f11bf8e8b47be6f75cd74840f64535d5388a
Kimberly Huynh and Sara Hamilton <sara@>
2018-01-16 14:23:31 -0800

    add javascript to return sorted array in all caps

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,16 @@ $(document).ready(function(){
   var groceries = [];
 
   $("#groceryForm").submit(function(event) {
-    groceries.push($("input#item1").val());
-      $("#groceryForm").trigger("reset");
+    if (($("input#item1").val()) != "") {
+      $(groceries.push($("input#item1").val().toUpperCase()));
+      };
+
+        var listItems = groceries.sort();
+        console.log(listItems);
+        $("#groceryForm").trigger("reset");
+
     console.log(groceries);
 
-
     event.preventDefault();
-
-
   });
 });
